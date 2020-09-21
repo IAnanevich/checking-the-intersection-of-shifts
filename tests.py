27c0f49356d0fccc5ccf798864ce3d6852b1e471
Ivan Ananevich <ivanshahter71@gmail.com>
2020-09-21 19:19:53 +0300

    with ddt

--- tests.py
+++ tests.py
@@ -1,52 +1,27 @@
 import unittest
 from shift_time import Shift
 from data_time import tests_false, tests_true
+from ddt import ddt, unpack, data
 
 
+@ddt
 class ShiftTest(unittest.TestCase):
-    def test_good(self):
-        shift_list, check_list = [], []
-        for shifts in tests_true:
-            shift1 = Shift(
-                time_from=shifts[0]["time_from"],
-                time_to=shifts[0]["time_to"],
-                date_from=shifts[0]["date_from"],
-                date_to=shifts[0]["date_to"],
-                week_days=shifts[0]["week_days"]
-            )
-            shift2 = Shift(
-                time_from=shifts[1]["time_from"],
-                time_to=shifts[1]["time_to"],
-                date_from=shifts[1]["date_from"],
-                date_to=shifts[1]["date_to"],
-                week_days=shifts[1]["week_days"]
-            )
-            shift_list.append(Shift.compare(shift1, shift2))
-            check_list.append(True)
 
-        self.assertEqual(shift_list, check_list)
+    @data(tests_false)
+    @unpack
+    def test_1(self, tuple1, tuple2):
+        @data(tuple1, tuple2)
+        @unpack
+        def test_bad(shift_1, shift_2):
+            self.assertFalse(Shift.compare(shift_1, shift_2))
 
-    def test_bad(self):
-        shift_list, check_list = [], []
-        for shifts in tests_false:
-            shift1 = Shift(
-                time_from=shifts[0]["time_from"],
-                time_to=shifts[0]["time_to"],
-                date_from=shifts[0]["date_from"],
-                date_to=shifts[0]["date_to"],
-                week_days=shifts[0]["week_days"]
-            )
-            shift2 = Shift(
-                time_from=shifts[1]["time_from"],
-                time_to=shifts[1]["time_to"],
-                date_from=shifts[1]["date_from"],
-                date_to=shifts[1]["date_to"],
-                week_days=shifts[1]["week_days"]
-            )
-            shift_list.append(Shift.compare(shift1, shift2))
-            check_list.append(False)
-
-        self.assertEqual(shift_list, check_list)
+    @data(tests_true)
+    @unpack
+    def test_2(self, *args):
+        @data(*args)
+        @unpack
+        def test_good(shift_1, shift_2):
+            self.assertTrue(Shift.compare(shift_1, shift_2))
 
 
 if __name__ == '__main__':
